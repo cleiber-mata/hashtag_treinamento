3b36b96dec01f3b3d778adec4dbb036d3d3f58a3
Cleiber Mata <cleiber.mata@gmail.com>
2025-12-01 01:38:31 -0300

    Projeto Completo

--- projeto_hashtag/projeto_aula.py
+++ projeto_hashtag/projeto_aula.py
@@ -4,6 +4,7 @@ import pandas as pd
 import time
 pg.PAUSE = 0.5
 link_drive = "https://drive.google.com/drive/folders/1X4qQW6ODmQlWei1Ei_YZiS_vuFyH96F8"
+email_link = r"https://mail.google.com/"
 
 # pg.click -> clicar
 # pg.press -> apertar 1 tecla
@@ -27,5 +28,6 @@ pg.click(x=365, y=480) # Selecionar o arquivo
 pg.click(x=1265, y=483) # Clicar nos 3 pontinhos
 time.sleep(3)
 pg.click(x=992, y=381) # Clicar em fazer o download
-
+pg.click(x=527, y=440) # Salvar como
+time.sleep(5)
 
